a22cb4e69160aa4a27fd6dc5f284d7ef3dab7f2f
ankitrekha01 <ankitrekha27@gmail.com>
2022-10-08 15:25:06 +0530

    modified

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useParams } from 'react-router-dom';
 
 function Employees(){
-    const { id } = useParams();1
+    const { id } = useParams();
     return(
         <div className="employees">
             <h1>{id}</h1>
