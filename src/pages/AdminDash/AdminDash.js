6d26f1caf353394e82d64b15d46fcf7bfbf4a781
Kartik237 <kartik237@gmail.com>
2022-10-08 16:20:08 +0530

    PopupForm Added

---
+++ src/pages/AdminDash/AdminDash.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import {Button, Alert, Row, Col} from 'react-bootstrap';
+import AdminDash from './Components/AdminDash';
+
+
+
+function App() {
+  return (
+    <div className="App">
+   <AdminDash/>
+
+        
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
