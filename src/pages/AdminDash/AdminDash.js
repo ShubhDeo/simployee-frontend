552f820aea3022479f232c2948f944ebd7048c6b
Kartik237 <kartik237@gmail.com>
2022-10-09 14:41:19 +0530

    Admin Functionality

--- src/pages/AdminDash/AdminDash.js
+++ src/pages/AdminDash/AdminDash.js
@@ -5,7 +5,12 @@ import Pagination from "../../Components/Pagination";
 import AdminLogo from "../../Components/AdminLogo";
 import AddEmployee from "../../Components/AddEmployee";
 
+
+
+
 function AdminDash() {
+  
+
   return (
     <div className="App">
       <>
@@ -13,19 +18,12 @@ function AdminDash() {
         <br />
 
         <div className="text-center">
-          <div className="Left" >
+          <div className="Left">
             <h4> Employees</h4>
             <AddEmployee />
             <br />
             <Pagination />
           </div>
-
-          {/* <div
-            className="right"
-            style={{ marginRight: "5%", marginTop: "5%", textAlign: "left" }}
-          >
-            <AdminLogo />
-          </div> */}
         </div>
       </>
     </div>
